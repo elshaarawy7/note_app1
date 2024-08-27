2b54c1da997709e4ae069660bcf82e940c082df3
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2024-08-27 10:35:38 +0300

    editni note view ui don

--- lib/views/edit_note_view_body.dart
+++ lib/views/edit_note_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/Custem_Text_Field.dart';
 import 'package:note_app/widgets/custem-apppar.dart';
 import 'package:note_app/widgets/custem_search_icon.dart';
 
@@ -9,7 +10,7 @@ class EditNoteViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 24),
-      child:  Column(  
+      child: const Column(  
         children: [
           SizedBox(
             height:50 , 
@@ -18,7 +19,25 @@ class EditNoteViewBody extends StatelessWidget {
             CustemAppBar(
               title: "Edit Note",
               icon: Icons.check,
-            ),
+            ),  
+
+           SizedBox(
+            height:50 , 
+            )  , 
+
+            CustomTextField(
+              hint:"Title"
+            ) , 
+
+            SizedBox(height: 16,) , 
+             
+
+            CustomTextField(
+              hint: "conect",
+              minLines: 5,
+            )
+
+
         ],
       ),
     );
